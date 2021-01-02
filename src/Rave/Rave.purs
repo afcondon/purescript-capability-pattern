960b9291f757a1a55cb79649bb6d71435950f607
Andrew Condon <andrew.condon@gmail.com>
2021-01-02 12:42:34 +0100

    clean up, add getPureScript example

--- src/Rave/Rave.purs
+++ src/Rave/Rave.purs
@@ -4,57 +4,42 @@ import Prelude
 
 import App.Layer.Four (Name(..))
 import App.Layer.Three (class GetUserName, class Logger)
-import Control.Monad.Error.Class (class MonadError, class MonadThrow, throwError, try)
+import Control.Monad.Error.Class (class MonadThrow, throwError)
 import Control.Monad.Except (ExceptT, runExceptT)
-import Control.Monad.Except.Checked (ExceptV, handleError, handleErrors, safe)
+import Control.Monad.Except.Checked (ExceptV, handleError, safe)
 import Control.Monad.Reader (class MonadAsk, ReaderT, ask, asks, runReaderT)
-import Control.Monad.Trans.Class (lift)
 import Data.Either (Either(..))
-import Data.Symbol (class IsSymbol, SProxy(..))
-import Data.Variant (class VariantShows, Variant, inj)
+import Data.Variant (class VariantShows, Variant)
 import Data.Variant.Internal (class VariantTags, RProxy(..))
-import Effect (Effect)
-import Effect.Aff (Aff, error, launchAff_)
-import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Aff (Aff, error)
+import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Class.Console as Console
-import Effect.Exception (Error)
-import Node.FS.Aff (writeTextFile)
 import Node.Path (FilePath)
-import Prim.Row as R
 import Prim.RowList (class RowToList)
-import Prim.RowList as RL
-import Rave.FS (class MonadFs, FsError, writeEV)
-import Rave.HTTP (class MonadHttp, HttpError, getEV, getEVasUnit)
-import Rave.RowTypes (type (+))
-import Record (get) as Record
+import Rave.FS (class MonadFs, writeEV)
+import Rave.HTTP (class MonadHttp, getEV)
 import Type.Data.Row (RProxy)
 import Type.Equality (class TypeEquals, from)
 
 -- | ...and the means to run computations in it
 runApp :: forall a. Rave Environment () a -> Environment -> Aff a
 runApp rave env = runRave (RProxy :: _ ()) env rave
-
--- the unified function 
--- getPureScript ∷ ∀ r env. Rave env (HttpError + FsError + r) Unit
-getPureScript :: forall m t133
-  . Monad m
-  => MonadHttp m
-  => MonadFs m
-  => ExceptT (Variant
-                ( fsFileNotFound     :: String
-                , fsPermissionDenied :: Unit
-                , httpNotFound    :: Unit
-                , httpOther       :: { status :: Int, body :: String }
-                , httpServerError :: String
-                | t133
-                )
-              )
-              m
-              Unit
-getPureScript = do
-  getEV "http://purescript.org" >>= writeEV "~/purescript.html"
-
+  where
+    runRave :: forall var env rl.
+      RowToList var rl =>
+      VariantTags rl =>
+      VariantShows rl =>
+      RProxy var ->
+      env ->
+      Rave env var a ->
+      Aff a
+    runRave _ env (Rave rave) = do
+      ran <- runExceptT $ runReaderT rave env
+      case ran of
+        Right res -> pure res
+        Left l -> throwError $ error $ show l
+    
 
 -- the type that we expect to be in the ReaderT as our environment
 type Environment = { raveEnv :: String }
@@ -86,9 +71,10 @@ instance getUserNameRave :: GetUserName (Rave env var) where
   getUserName = do
     env <- ask -- we still have access to underlying ReaderT
 
-    resultHttp <- safe $ (getEV "test") # handleError errorHandlersBundle
+    resultHttp <- safe $ getEV "test" # handleError errorHandlersBundle
+    safe $ getPureScript # handleError errorHandlersBundle2
   
-    pure $ Name "sort out the types first, then we'll see about names"
+    pure $ Name resultHttp
 
 {- 
 
@@ -108,6 +94,23 @@ possiblyFailingCode _ = do
   pure x 
   
 -}
+
+-- the unified function 
+-- getPureScript ∷ ∀ r env. Rave env (HttpError + FsError + r) Unit
+getPureScript :: forall m t133
+  . Monad m
+  => MonadHttp m
+  => MonadFs m
+  => ExceptV ( fsFileNotFound     :: String
+                , fsPermissionDenied :: Unit
+                , httpNotFound    :: Unit
+                , httpOther       :: { status :: Int, body :: String }
+                , httpServerError :: String
+                | t133
+                ) m Unit
+getPureScript = do
+  getEV "http://purescript.org" >>= writeEV "~/purescript.html"
+
 
 errorHandlersBundle :: forall m.
   MonadEffect m =>
@@ -135,60 +138,29 @@ errorHandlersBundle = {
       pure "foo"
 }
 
+errorHandlersBundle2:: forall m.
+  MonadEffect m =>
+  { fsFileNotFound     :: FilePath -> m Unit
+  , fsPermissionDenied :: Unit -> m Unit
+  , httpNotFound       :: Unit -> m Unit
+  , httpOther          :: { body :: String, status :: Int} -> m Unit
+  , httpServerError    :: String -> m Unit
+  }
+errorHandlersBundle2 = {
+    httpServerError:    \error -> do
+      Console.log $ "Server error:" <> error
+      pure unit
+  , httpNotFound:       \error -> do
+      Console.log "Not found"
+      pure unit
+  , httpOther:          \error -> do
+      Console.log $ "Other: { status: " <> show error.status <> " , body: " <> error.body <> "}"
+      pure unit
+  , fsFileNotFound:     \error -> do
+      Console.log $ "File Not Found" <> error
+      pure unit
+  , fsPermissionDenied: \error -> do
+      Console.log "Permission Denied"
+      pure unit
+}
 
--- original Rave machinery below
-
-class VariantInjTagged a b | a -> b where
-  injTagged :: Record a -> Variant b
-
-instance variantInjTagged ::
-  ( RowToList r1 (RL.Cons sym a RL.Nil)
-  , R.Cons sym a () r1
-  , R.Cons sym a rx r2
-  , IsSymbol sym
-  ) =>
-  VariantInjTagged r1 r2 where
-    injTagged = inj (SProxy :: SProxy sym) <<< Record.get (SProxy :: SProxy sym)
-
-throw :: forall m r1 r2 a.
-  VariantInjTagged r1 r2 =>
-  MonadThrow (Variant r2) m =>
-  Record r1 ->
-  m a
-throw = throwError <<< injTagged
-
-runRave :: forall var env rl a.
-  RowToList var rl =>
-  VariantTags rl =>
-  VariantShows rl =>
-  RProxy var ->
-  env ->
-  Rave env var a ->
-  Aff a
-runRave _ env (Rave rave) = do
-  ran <- runExceptT $ runReaderT rave env
-  case ran of
-    Right res -> pure res
-    Left l -> throwError $ error $ show l
-
-liftRave :: forall m a r. MonadError Error m => m a -> ExceptV (liftedError :: Error | r) m a
-liftRave e = do
-  run <- lift $ try e
-  case run of
-    Right r -> pure r
-    Left l -> throw { liftedError: l }
-
-liftAffV :: forall r m a. MonadAff m => Aff a -> ExceptV (liftedError :: Error | r) m a
-liftAffV e = do
-  run <- liftAff $ try e
-  case run of
-    Right r -> pure r
-    Left l -> throw { liftedError: l }
- 
-
--- itV :: forall r.
---   RProxy r
---   String ->
---   ExceptV r Aff Unit ->
---   Spec Unit
--- itV name toRun = it name $ runAffV RProxy toRun
